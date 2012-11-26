75596ef599234eee21c5ea81c88926e40b35decf
Arek Wilk <veelckoo@tlen.pl>
2012-11-26 13:47:09 +0000

    Main class added

--- gjExcercise/src/com/southwestcode/gamejam1/Main.java
+++ gjExcercise/src/com/southwestcode/gamejam1/Main.java
@@ -1,13 +1,40 @@
 package com.southwestcode.gamejam1;
 
-public class Main {
+import javax.swing.JFrame;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("It's ALIVE!");
+public class Main extends JFrame {
+	private static final long serialVersionUID = 1L; // to stop eclipse complain
+	private final int WIDTH = 800;
+	private final int HEIGHT = 600;
+	private final String TITLE = "Eat and don't die";
+
+	public static Main instance = null;
+
+	private Main() {
+		super(); // construct a new invisible JFrame
+		/* Define the frame properties */
+		add(new Board());
+		
+		this.setSize(this.WIDTH, this.HEIGHT);
+		this.setTitle(TITLE);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
 	}
 
+	public static Main getInstance() {
+		if (instance == null) {
+			instance = new Main();
+		}
+		return instance;
+	}
+
+	/* End of singleton code */
+
+	public static void main(String[] args) {
+		System.out.println("It is ALIVE!");
+		Main.getInstance();
+		
+	}
 }
