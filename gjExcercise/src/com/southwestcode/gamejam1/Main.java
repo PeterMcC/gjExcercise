2cfb75f35ba3fe0688d5512cd6f22a334681939d
Arek Wilk <veelckoo@tlen.pl>
2012-11-26 10:53:35 +0000

    Initial commit

---
+++ gjExcercise/src/com/southwestcode/gamejam1/Main.java
@@ -0,0 +1,13 @@
+package com.southwestcode.gamejam1;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It's ALIVE!");
+	}
+
+}
